b1f54e0d1009b40dfc93615ba17c818e41e16940
Admin <Admin@admin.com>
2025-03-20 13:32:16 +0530

    Check

--- Modules/ApplicationEvents.vb
+++ Modules/ApplicationEvents.vb
@@ -12,7 +12,6 @@
         'Protected Overrides Function OnInitialize(ByVal commandLineArgs As System.Collections.ObjectModel.ReadOnlyCollection(Of String)) As Boolean
         '   Me.MinimumSplashScreenDisplayTime = 2000 'Milliseconds
         '  Return MyBase.OnInitialize(commandLineArgs)
-
         'End Function
     End Class
 End Namespace
